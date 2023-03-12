c7bbcee38ea8d52d853452679ad9c0f17e2db9f6
Luke <116524469+lkadieguez@users.noreply.github.com>
2023-03-12 14:31:37 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,21 @@
-/*
-  OBJETIVO - quando clicarmos no botão temos que mostrar a imagem de fundo correspondente
+// agrupar todos os botões e imagens de fundo
+// identificar o botão clicado
+// desmarcar o botão anterior
+// marcar o botão clicado
+// desativar a imagem atual
+// ativar a imagem correspondente ao botão clicado
 
-  // passo 1 - dar um jeito de pegar o elemento HTML dos botões
-  // passo 2 - dar um jeito de identificar o clique do usuário no botão
-  // passo 3 - desmarcar o botão selecionado anterior
-  // passo 4 - marcar o botão clicado como se estivesse selecionada
-  // passo 5 - esconder a imagem do fundo anterior
-  // passo 6 - fazer aparecer a imagem de fundo correspondente ao botão clicado
-*/
+const btnArray = document.querySelectorAll(".button");
+const imgArray = document.querySelectorAll(".image");
 
-const botoesCarrossel = document.querySelectorAll(".botao");
-const imagensCarrossel = document.querySelectorAll(".imagem");
+btnArray.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    document.querySelector(".actived").classList.remove("actived");
 
-botoesCarrossel.forEach((botao, i) => {
-  botao.addEventListener("click", () => {
-    DesativarBotaoSelecionado();
+    btn.classList.add("actived");
 
-    MarcarBotaoSelecionado(botao);
+    document.querySelector(".show").classList.remove("show");
 
-    EsconderImagemAtiva();
-
-    MostrarImagemAtiva(i);
+    imgArray[i].classList.add("show");
   });
 });
-
-function MostrarImagemAtiva(i) {
-  imagensCarrossel[i].classList.add("ativa");
-}
-
-function MarcarBotaoSelecionado(botao) {
-  botao.classList.add("selecionado");
-}
-
-function EsconderImagemAtiva() {
-  const imagemAtiva = document.querySelector(".ativa");
-  imagemAtiva.classList.remove("ativa");
-}
-
-function DesativarBotaoSelecionado() {
-  const botaoSelecionado = document.querySelector(".selecionado");
-  botaoSelecionado.classList.remove("selecionado");
-}
